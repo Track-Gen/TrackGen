7e490c0df0f096471b9c6d053ca6e66bb034b70c
Coding Cactus <65423356+Coding-Cactus@users.noreply.github.com>
2021-08-09 16:03:54 +0000

    🛠️: switched bt files to using larger map

--- static/js/pages.js
+++ static/js/pages.js
@@ -10,5 +10,9 @@ document.querySelectorAll("#input-type button").forEach(button => {
 
 		document.querySelectorAll("#input-type button").forEach(button => button.className="");
 		button.className = "selected";
+
+		setTimeout(() => {
+			alert("Attempting to generate large track maps can cause a lot of stress on the server and take lengthy periods of time to complete. If you are trying to generate a large image, please consider using the desktop app.");
+		}, 1000);
 	});
 });
